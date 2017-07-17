598f7d672b6a652cccd212d4c8085fa7faa8027e
stiag0 <stiago01r@gmail.com>
2017-07-17 12:43:28 -0500

    correccion
    arreglo una funcion en java waifu, ya si regresa la hora actual.

--- State/javaWaifu/src/javawaifu/Feliz.java
+++ State/javaWaifu/src/javawaifu/Feliz.java
@@ -4,19 +4,24 @@ las funciones en cada uno de los estados, estos estados junto a la interfaz son 
 ya que asi nos ahorramos el usar variables y una infinidad de condicionales en el codigo.
  */
 package javawaifu;
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
 /**
  *
- * @author stiagoo
+ * @author stiago
  */
 public class Feliz implements Estado {
+
     @Override
     public void preguntarComoEsta(){
         System.out.println("muy bien, feliz de verte :3");
     }
     public void preguntarPorLahora(){
-        long tiempo = System.currentTimeMillis();
-        System.out.println(tiempo+"  milisegundos");
+        
+        Date ahora = new Date();
+        SimpleDateFormat formateador = new SimpleDateFormat("hh:mm:ss");
+        
+        System.out.println("waifu: "+formateador.format(ahora));
     }
     public void preguntarQueVaAHacerMasTarde(){
         System.out.println("Lo que tu quieras, guapo :P");
